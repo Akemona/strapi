349e9dd858541efd11caabd477d488b8b888bd81
Akash P <aksdevac@gmail.com>
2023-01-17 16:36:51 +0530

    update e2e test config

--- jest.config.e2e.js
+++ jest.config.e2e.js
@@ -1,8 +1,9 @@
 module.exports = {
-  name: 'API integration tests',
+  displayName: 'API integration tests',
   testMatch: ['**/?(*.)+(spec|test).e2e.js'],
   testEnvironment: 'node',
   setupFilesAfterEnv: ['<rootDir>/test/jest2e2.setup.js'],
+  testTimeout: 100000,
   coveragePathIgnorePatterns: [
     '<rootDir>/dist/',
     '<rootDir>/node_modules/',
