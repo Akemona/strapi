d6e063398f014773c8bfe29d26deaa72a3f47b49
Akash <2557058+aksdevac@users.noreply.github.com>
2024-02-15 10:57:51 +0530

    WIP: node 20 support

--- packages/strapi-generate-new/lib/resources/json/package.json.js
+++ packages/strapi-generate-new/lib/resources/json/package.json.js
@@ -44,7 +44,7 @@ module.exports = (opts) => {
       ...packageJsonStrapi,
     },
     engines: {
-      node: '>=14.19.1 <=18.x.x',
+      node: '>=18.17.0 <=20.x.x',
       npm: '^6.0.0',
     },
     license: 'MIT',
